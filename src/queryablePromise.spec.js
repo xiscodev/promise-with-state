d1c603fce0f40bf5e792ad24eb139a8affb550b3
Jose Diaz <xoxefdp@gmail.com>
2021-07-24 10:31:21 +0200

    Added tests for QueryablePromise class

---
+++ src/queryablePromise.spec.js
@@ -0,0 +1,130 @@
+import QueryablePromise from 'queryablePromise';
+import { FULFILLED, PENDING, REJECTED } from 'state';
+
+// ENVIRONMENT VARIABLES
+jest.useFakeTimers()
+
+const waitTime = 2000
+
+let queryablePromiseResolve = new QueryablePromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve()
+  }, 1000)
+})
+
+let queryablePromiseReject = new QueryablePromise((resolve, reject) => {
+  setTimeout(() => {
+    reject()
+  }, 1000)
+})
+
+const _resetEnv = () => {
+  jest.clearAllTimers()
+
+  queryablePromiseResolve = new QueryablePromise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+
+  queryablePromiseReject = new QueryablePromise((resolve, reject) => {
+    setTimeout(() => {
+      reject()
+    }, 1000)
+  })
+}
+
+//  TESTS
+describe('QueryablePromise', () => {
+  it('should exist', () => {
+    expect(QueryablePromise).not.toBeUndefined()
+  })
+
+  it('should has resolve static function', () => {
+    expect(typeof QueryablePromise.resolve).toBe('function')
+  })
+
+  it('should has reject static function', () => {
+    expect(typeof QueryablePromise.reject).toBe('function')
+  })
+
+  it('should be instantiable', () => {
+    const queryable = new QueryablePromise()
+    expect(typeof queryable).toBe('object')
+  })
+
+  it('should has state property after makeQueryablePromise', () => {
+    expect(queryablePromiseResolve.state).toBeDefined()
+  })
+
+  it('should has isPending function after makeQueryablePromise', () => {
+    expect(typeof queryablePromiseResolve.isPending).toBe('function')
+  })
+
+  it('should has isFulfilled function after makeQueryablePromise', () => {
+    expect(typeof queryablePromiseResolve.isFulfilled).toBe('function')
+  })
+
+  it('should has isRejected function after makeQueryablePromise', () => {
+    expect(typeof queryablePromiseResolve.isRejected).toBe('function')
+  })
+
+  it('should thenable on pending state be PENDING', () => {
+    expect(queryablePromiseResolve.state).toBe(PENDING)
+  })
+
+  it('should thenable on fulfill state be FULFILLED', () => {
+    _resetEnv()
+
+    queryablePromiseResolve
+      .then(() => {
+        expect(queryablePromiseResolve.state).toBe(FULFILLED)
+      })
+      .catch()
+    
+    jest.advanceTimersByTime(waitTime)
+  })
+
+
+  it('should thenable on reject state be REJECTED', () => {
+    _resetEnv()
+
+    queryablePromiseReject
+    .then()
+    .catch(() => {
+      expect(queryablePromiseReject.state).toBe(REJECTED)
+    })
+
+    jest.advanceTimersByTime(waitTime)
+  })
+
+  it('should exist isPending method and return true on PENDING', () => {
+    _resetEnv()
+
+    expect(queryablePromiseResolve.isPending()).toBeTruthy()
+  })
+
+  it('should exist isFulfilled method and return true on FULFILLED', () => {
+    _resetEnv()
+
+    queryablePromiseResolve
+      .then(() => {
+        expect(queryablePromiseResolve.isFulfilled()).toBeTruthy()
+      })
+      .catch()
+    
+    jest.advanceTimersByTime(waitTime)
+  })
+
+  it('should exist isRejected method and return true on REJECTED', () => {
+    _resetEnv()
+
+    queryablePromiseReject
+    .then()
+    .catch(() => {
+      expect(queryablePromiseReject.isRejected()).toBeTruthy()
+    })
+
+    jest.advanceTimersByTime(waitTime)
+  })
+})
